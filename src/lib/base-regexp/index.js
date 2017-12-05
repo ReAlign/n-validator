78b5f95cb8194dff000667ad2e7abb8dbd0cde53
ReAlign <realign@yeah.net>
2017-12-05 22:04:19 +0800

    re:init

---
+++ src/lib/base-regexp/index.js
@@ -0,0 +1,5 @@
+let BaseRegExp = {
+    int: () => /^(?:[-+]?(?:0|[1-9][0-9]*))$/
+};
+
+export default BaseRegExp;
---
+++ src/lib/number/is-int.js
@@ -0,0 +1,8 @@
+import _re from './../base-regexp/index';
+
+module.exports = ((_) => {
+    _.isInt = (num) => {
+        console.log(num);
+        console.log(_re);
+    };
+});
