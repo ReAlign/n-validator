0bb3a55404fd496e36b6e534af15bb7a5bdaa160
ReAlign <realign@yeah.net>
2017-12-09 00:02:27 +0800

    re:add data uri

---
+++ __test__/uri/is-data-uri.test.js
@@ -0,0 +1,17 @@
+const _ = require('./../../index');
+
+const str1 = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUAAAD///+l2Z/dAAAAM0lEQVR4nGP4/5/h/1+G/58ZDrAz3D/McH8yw83NDDeNGe4Ug9C9zwz3gVLMDA/A6P9/AFGGFyjOXZtQAAAAAElFTkSuQmCC';
+const str2 = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAIBAMAAAA2IaO4AAAAFVBMVEXk5OTn5+ft7e319fX29vb5+fn///++GUmVAAAALUlEQVQIHWNICnYLZnALTgpmMGYIFWYIZTA2ZFAzTTFlSDFVMwVyQhmAwsYMAKDaBy0axX/iAAAAAElFTkSuQmCC';
+const str3 = '   data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAIBAMAAAA2IaO4AAAAFVBMVEXk5OTn5+ft7e319fX29vb5+fn///++GUmVAAAALUlEQVQIHWNICnYLZnALTgpmMGYIFWYIZTA2ZFAzTTFlSDFVMwVyQhmAwsYMAKDaBy0axX/iAAAAAElFTkSuQmCC   ';
+
+test('normalDataURI1', function() {
+    expect(_.isDataURI(str1)).toBe(true);
+});
+
+test('normalDataURI2', function() {
+    expect(_.isDataURI(str2)).toBe(true);
+});
+
+test('normalDataURI3', function() {
+    expect(_.isDataURI(str3)).toBe(true);
+});
--- src/lib/base-regexp/index.js
+++ src/lib/base-regexp/index.js
@@ -7,7 +7,6 @@ module.exports = ((_) => {
         time: /^(20|21|22|23|[0-1]\d):[0-5]\d:[0-5]\d$/,
         dateTime: (separator = '-', strictMode = true) => {
             let strictStr = strictMode ? '{1}' : '+';
-            console.log(strictStr);
             /*eslint-disable*/
             return new RegExp(`^[1-9]\\d{3}${separator}(0[1-9]|1[0-2])${separator}(0[1-9]|[1-2][0-9]|3[0-1])\\s${strictStr}(20|21|22|23|[0-1]\\d):[0-5]\\d:[0-5]\\d$`);
             /*eslint-enable*/
